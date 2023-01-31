c7559965d61f7cdfc825810162b68f4ee81609db
Mike Lebedev <to-mike@ya.ru>
2023-01-31 16:05:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // цена билета
+        int bilet = 749;
+
+        // переменная для подсчета количества миль
+        int bonus = 20;
+
+        // переменная с количеством миль
+        int mile = bilet / bonus;
+
+        // вывод результата
+        System.out.println(mile);
+
+    }
+}
